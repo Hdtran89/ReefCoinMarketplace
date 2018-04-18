4082cb380c12eb63246cf55e6073a833c6174fe8
Hieu Tran <hieutran@Hieus-MacBook-Pro-2.local>
2018-04-18 09:31:31 -0700

    first init commit

---
+++ App.js
@@ -0,0 +1,105 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput
+} from 'react-native';
+
+import Amplify, { Auth } from 'aws-amplify'
+
+import config from './src/aws-export'
+
+Amplify.configure(config)
+
+type Props = {};
+export default class App extends Component<Props> {
+  state = { // 1
+    authCode: '',
+    user: {}
+  }
+  onChangeText(authCode) { // 2
+    this.setState({ authCode })
+  }
+  signUp() {
+    Auth.signUp({ // 3
+      username: 'myCoolUsername',
+      password: 'MyCoolP@ssword2!',
+      attributes: {
+        phone_number: '+15555555555',
+        email: 'yourcoolemail@gmail.com'
+      }
+    })
+    .then(res => {
+      console.log('successful signup: ', res)
+    })
+    .catch(err => {
+      console.log('error signing up: ', err)
+    })
+  }
+  confirmUser() { // 4
+    const { authCode } = this.state
+    Auth.confirmSignUp('myCoolUsername', authCode)
+      .then(res => {
+        console.log('successful confirmation: ', res)
+      })
+      .catch(err => {
+        console.log('error confirming user: ', err)
+      })
+  }
+  signIn() { // 1
+    Auth.signIn(username, password)
+      .then(user => {
+      // save user in state somewhere
+      this.setState({ user })
+      })
+      .catch(err => {
+        console.log('error signing in: ', err)
+      })
+  }
+  confirmSignIn() { // 2
+    Auth.confirmSignIn(user, authCode)
+      .then(user => {
+        console.log('user: ', user)
+      }).catch(err => {
+        console.log('error confirming sign in: ', err)
+      })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title='Sign Up' onPress={this.signUp.bind(this)} />
+        <TextInput
+          placeholder='Input Code'
+          onChangeText={value => this.onChangeText(value)}
+          style={styles.input}
+        />
+        <Button
+          title='Confirm User'
+          onPress={this.confirmUser.bind(this)}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 50,
+    backgroundColor: '#ededed',
+    marginVertical: 10
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF'
+  }
+})
---
+++ README.md
@@ -0,0 +1,7 @@
+# ReefCoinMarketplace
+
+#How to contribute
+1. git clone `https://github.com/Hdtran89/ReefCoinMarketplace.git`
+2. npm install
+3. turn on emulator for ios or android
+4. react-native run-android(android) or react-native run-ios(android)
